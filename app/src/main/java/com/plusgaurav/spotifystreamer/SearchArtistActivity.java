d02447b134357c52ee496c436601f2dc60369d40
gaurav91pandey <gaurav91pandey@gmail.com>
2015-06-03 10:50:21 -0700

    added authentication logic and search artist logic

--- app/src/main/java/com/plusgaurav/spotifystreamer/SearchArtistActivity.java
+++ app/src/main/java/com/plusgaurav/spotifystreamer/SearchArtistActivity.java
@@ -1,18 +1,74 @@
 package com.plusgaurav.spotifystreamer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 public class SearchArtistActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "33bbc039194a4921a55a18dedc5bb329";
+    private static final String REDIRECT_URI = "spotifystreamer://callback";
+    private static String accessToken;
+    private static final int REQUEST_CODE = 1337;
+
+
+    public static String getAccessToken() {
+        return accessToken;
+    }
+
+    private static void setAccessToken(String accessToken) {
+        SearchArtistActivity.accessToken = accessToken;
+    }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_artist);
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    Toast.makeText(SearchArtistActivity.this, "Logged in!", Toast.LENGTH_SHORT).show();
+                    setAccessToken(response.getAccessToken());
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/plusgaurav/spotifystreamer/SearchArtistActivityFragment.java
+++ app/src/main/java/com/plusgaurav/spotifystreamer/SearchArtistActivityFragment.java
@@ -1,18 +1,25 @@
 package com.plusgaurav.spotifystreamer;
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import kaaes.spotify.webapi.android.models.Pager;
+
 public class SearchArtistActivityFragment extends Fragment {
 
     private EditText searchArtist;
@@ -23,15 +30,66 @@ public class SearchArtistActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = (View) inflater.inflate(R.layout.fragment_search_artist, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_search_artist, container, false);
+
+        // Listener for when the user is done typing the artist name in the edittext feild
         searchArtist = (EditText) rootView.findViewById(R.id.searchArtist);
         searchArtist.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                Toast.makeText(getActivity(),"Hola!",Toast.LENGTH_LONG).show();
-                return true;
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+
+                    // hide virtual keyboard
+                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(searchArtist.getWindowToken(), 0);
+
+                    // TODO: search for artists
+                    FetchArtistTask task = new FetchArtistTask();
+                    task.execute(searchArtist.getText().toString());
+                    Toast.makeText(getActivity(), "Hola!", Toast.LENGTH_LONG).show();
+
+                    return true;
+                }
+                return false;
             }
         });
+
         return rootView;
+    }
+
+    public class FetchArtistTask extends AsyncTask<String, Void, String[]> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String[] doInBackground(String... artistName) {
+
+            String[] result = new String[10];
+
+            // do spotify transaction
+            SpotifyApi api = new SpotifyApi();
+            api.setAccessToken(SearchArtistActivity.getAccessToken());
+            SpotifyService spotify = api.getService();
+
+            // get artist
+            ArtistsPager artistsPager = spotify.searchArtists(artistName[0]);
+
+            Pager<Artist> artist = artistsPager.artists;
+
+
+            return result;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+        }
     }
 }
