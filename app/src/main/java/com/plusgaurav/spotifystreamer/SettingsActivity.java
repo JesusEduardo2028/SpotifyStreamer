03eee577448a47e38bc273587b91ea0e9d0d9272
gaurav91pandey <gaurav91pandey@gmail.com>
2015-06-04 22:05:47 -0700

    fixed setting preferences bug

--- app/src/main/java/com/plusgaurav/spotifystreamer/SettingsActivity.java
+++ app/src/main/java/com/plusgaurav/spotifystreamer/SettingsActivity.java
@@ -1,15 +1,62 @@
 package com.plusgaurav.spotifystreamer;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
 
-// TODO update settings to non depricated one
-// TODO add actionbar in settings
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends ActionBarActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
     }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle paramBundle) {
+            super.onCreate(paramBundle);
+
+            // get options from resource file
+            addPreferencesFromResource(R.xml.pref_general);
+
+            // bind changes to value
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.user_type_key)));
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+
+            preference.setOnPreferenceChangeListener(preference.getOnPreferenceChangeListener());
+
+            onPreferenceChange(preference,
+                    PreferenceManager
+                            .getDefaultSharedPreferences(preference.getContext())
+                            .getString(preference.getKey(), ""));
+        }
+
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    preference.setSummary(listPreference.getEntries()[prefIndex]);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+
+
+    }
+
 }
